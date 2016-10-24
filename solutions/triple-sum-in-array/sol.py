af2a688914fe116aeda1dc76da9dc588b8d468a8
flo <theflorishone@gmail.com>
2016-10-24 23:50:16 +0200

    fixed python triple-sum solution

--- solutions/triple-sum-in-array/sol.py
+++ solutions/triple-sum-in-array/sol.py
@@ -1,26 +1,25 @@
+#!/bin/env python2
+
 t = int(raw_input())
 res = 0
 
 while t>0:
-    dict={}
-    res=0
+    res = 0
     n,x = map(int,raw_input().split())
     arr = map(int,raw_input().split())
     arr.sort()
-    for i in arr:
-        dict[i]=True
     for i in range(0,n-2):
-        if res==1:
+        if res == 1:
             break
-        l=i+1
-        r=n-1
+        l = i+1
+        r = n-1
         while l < r:
-            if arr[i]+arr[l]+arr[r] == sum:
-                res=1
+            if arr[i]+arr[l]+arr[r] == x:
+                res = 1
                 break
-            elif arr[i]+arr[l]+arr[r] < sum:
-                l=l+1
+            elif arr[i]+arr[l]+arr[r] < x:
+                l = l+1
             else:
-                r=r-1
+                r = r-1
     print res
-    t=t-1
+    t = t-1
