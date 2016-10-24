b8c54c173cbb9fa598f99942dbffd3eb5f44e1d9
Gillett Hernandez <gillett.hernandez@gmail.com>
2016-10-24 11:46:27 -0700

    move tests into test function

--- solutions/linked-list-node-count/node_count.py
+++ solutions/linked-list-node-count/node_count.py
@@ -15,15 +15,19 @@ def node_count(node):
         c += 1
     return c
 
-first = Node(0)
-print(node_count(first))
-node = Node(1)
-first.next = node
-print(node_count(first))
-for i in range(3):
-    node.next = Node(i+2)
-    node = node.next
+def test():
+    first = Node(0)
     print(node_count(first))
-# detects loops
-node.next = first
-print(node_count(first))
+    node = Node(1)
+    first.next = node
+    print(node_count(first))
+    for i in range(3):
+        node.next = Node(i+2)
+        node = node.next
+        print(node_count(first))
+    # detects loops
+    node.next = first
+    print(node_count(first))
+
+if __name__ == '__main__':
+    test()
